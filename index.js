40726e1737376ec92ac8e86056b0843124c1fe55
Luciano Bueno <glamero@gmail.com>
2018-06-13 14:11:37 -0300

    bump

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
+var extract_data_os = function(doc){
+	let keys = {}
+	let path = ''
+	let host = ''
+
+	if(Array.isArray(doc)){
+		Object.each(doc[0].doc.data, function(data, key){
+			keys[key] = []
+		})
+
+		path = doc[0].doc.metadata.path.replace(/\./g, '/')
+		host = doc[0].doc.metadata.host
+
+		Array.each(doc, function(item){
+			Object.each(item.doc.data, function(value, key){
+				let data = {value: value, timestamp: item.doc.metadata.timestamp}
+				if(keys[key])
+					keys[key].push(data)
+			})
+		}.bind(this))
+	}
+	else {
+		Object.each(doc.data, function(data, key){
+			keys[key] = null
+		})
+		path = doc.metadata.path.replace(/\./g, '/')
+		host = doc.metadata.host
+
+		Object.each(doc.data, function(value, key){
+			let data = {value: value, timestamp: doc.metadata.timestamp}
+			keys[key] = data
+		})
+	}
+	return {keys: keys, path: path, host: host}
+}
+	
 module.exports = {
-	extract_data_os: function(doc){
-		let keys = {}
-		let path = ''
-		let host = ''
-
-		if(Array.isArray(doc)){
-			Object.each(doc[0].doc.data, function(data, key){
-				keys[key] = []
-			})
-
-			path = doc[0].doc.metadata.path.replace(/\./g, '/')
-			host = doc[0].doc.metadata.host
-
-			Array.each(doc, function(item){
-				Object.each(item.doc.data, function(value, key){
-					let data = {value: value, timestamp: item.doc.metadata.timestamp}
-					if(keys[key])
-						keys[key].push(data)
-				})
-			}.bind(this))
-		}
-		else {
-			Object.each(doc.data, function(data, key){
-				keys[key] = null
-			})
-			path = doc.metadata.path.replace(/\./g, '/')
-			host = doc.metadata.host
-
-			Object.each(doc.data, function(value, key){
-				let data = {value: value, timestamp: doc.metadata.timestamp}
-				keys[key] = data
-			})
-		}
-		return {keys: keys, path: path, host: host}
-	},
+	extract_data_os: extract_data_os,
 	
 	extract_data_os_historical: function (doc){
 		let type = ''
