a01af9828a280c491c7fdc4986139b7cdede88b4
Luciano Bueno <glamero@gmail.com>
2018-08-29 16:12:30 -0300

    bump

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+//var _array_path_to_keys = function(path, keys){
+	//let obj = {}
+	//if(Array.isArray(path) && path.length > 1){
+		//let first = path.shift()
+		//obj[first] = keys
+		//return _array_path_to_keys(path, obj)
+	//}
+	//else if(Array.isArray(path)){
+		//obj[path[0]] = keys
+		//return obj
+	//}
+	//else{
+		//obj[path] = keys
+		//return obj
+	//}
+//}
+
 var extract_data_os = function(doc){
 	let keys = {}
 	let path = ''
@@ -8,7 +25,9 @@ var extract_data_os = function(doc){
 			keys[key] = []
 		})
 
-		path = doc[0].doc.metadata.path.replace(/\./g, '/')
+		// path = doc[0].doc.metadata.path.replace(/\./g, '/')
+		path = doc[0].doc.metadata.path.replace(/\./g, '_')
+		// path = doc[0].metadata.path.split('.')
 		host = doc[0].doc.metadata.host
 
 		Array.each(doc, function(item){
@@ -23,7 +42,9 @@ var extract_data_os = function(doc){
 		Object.each(doc.data, function(data, key){
 			keys[key] = null
 		})
-		path = doc.metadata.path.replace(/\./g, '/')
+		// path = doc.metadata.path.replace(/\./g, '/')
+		path = doc.metadata.path.replace(/\./g, '_')
+		// path = doc.metadata.path.split('.')
 		host = doc.metadata.host
 
 		Object.each(doc.data, function(value, key){
@@ -31,15 +52,30 @@ var extract_data_os = function(doc){
 			keys[key] = data
 		})
 	}
+
+	// if(path.length > 1){
+  //
+	// 	let tmp_path = Array.clone(path)
+	// 	path = tmp_path.shift()
+	// 	let tmp_keys = Object.clone(keys)
+  //
+  //
+	// 	keys = _array_path_to_keys(tmp_path, tmp_keys)
+	// }
+	// else{
+	// 	path = path[0]
+	// }
+
+
 	return {keys: keys, path: path, host: host}
 }
-	
+
 module.exports = {
 	/**
 	* from os.stats.vue
 	**/
 	extract_data_os: extract_data_os,
-	
+
 	extract_data_os_historical: function (doc){
 		let type = ''
 		let range = {}
